3b5c36aa0d20cb91a3a66ab89e9c99d63810adef
DeltaResero <deltaresero@zoho.com>
2013-12-16 21:07:36 -0400

    Delay going tickless when cpu is loaded

--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -323,7 +323,7 @@ static void tick_nohz_stop_sched_tick(struct tick_sched *ts)
 	} while (read_seqretry(&xtime_lock, seq));
 
 	if (rcu_needs_cpu(cpu) || printk_needs_cpu(cpu) ||
-	    arch_needs_cpu(cpu)) {
+		arch_needs_cpu(cpu) || this_cpu_load()) {
 		next_jiffies = last_jiffies + 1;
 		delta_jiffies = 1;
 	} else {
