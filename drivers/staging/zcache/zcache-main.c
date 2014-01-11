24de3696eee5832d33cacec95165242794b470cf
DeltaResero <deltaresero@zoho.com>
2014-01-10 23:53:58 -0400

    Fix cleancache crash

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -1224,8 +1224,7 @@ static int zcache_pampd_get_data_and_free(char *data, size_t *bufsize, bool raw,
 					struct tmem_oid *oid, uint32_t index)
 {
 	BUG_ON(!is_ephemeral(pool));
-	if (zbud_decompress((struct page *)(data), pampd) < 0)
-		return -EINVAL;
+	zbud_decompress((struct page *)(data), pampd);
 	zbud_free_and_delist((struct zbud_hdr *)pampd);
 	atomic_dec(&zcache_curr_eph_pampd_count);
 	return 0;
