d9443ad42461fb4599eb9582ddc4890c0769fe0c
DeltaResero <deltaresero@zoho.com>
2014-01-10 22:39:42 -0400

    Scheduler uid id tweak

--- kernel/sys.c
+++ kernel/sys.c
@@ -605,6 +605,7 @@ static int set_user(struct cred *new)
 
 	free_uid(new->user);
 	new->user = new_user;
+	sched_autogroup_create_attach(current);
 	return 0;
 }
 
@@ -1114,7 +1115,7 @@ out:
 	write_unlock_irq(&tasklist_lock);
 	if (err > 0) {
 		proc_sid_connector(group_leader);
-		sched_autogroup_create_attach(group_leader);
+		
 	}
 	return err;
 }
