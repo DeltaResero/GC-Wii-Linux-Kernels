17acb2fa03ee0d93972f6db2eee10b8228f293cc
DeltaResero <deltaresero@zoho.com>
2014-01-11 01:06:47 -0400

    Decrease dirty page write ratio

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -78,7 +78,7 @@ int vm_highmem_is_dirtyable;
 /*
  * The generator of dirty data starts writeback at this percentage
  */
-int vm_dirty_ratio = 10;
+int vm_dirty_ratio = 1;
 
 /*
  * vm_dirty_bytes starts at 0 (disabled) so that it is a function of
