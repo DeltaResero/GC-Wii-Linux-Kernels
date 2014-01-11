2b4b32060130763da79c9fdcaf0c23e37da521fb
DeltaResero <deltaresero@zoho.com>
2014-01-11 00:24:14 -0400

    Make vfs_cache_pressure configurable

--- fs/dcache.c
+++ fs/dcache.c
@@ -76,7 +76,7 @@
  *   dentry1->d_lock
  *     dentry2->d_lock
  */
-int sysctl_vfs_cache_pressure __read_mostly = 100;
+int sysctl_vfs_cache_pressure __read_mostly = CONFIG_VFS_CACHE_PRESSURE;
 EXPORT_SYMBOL_GPL(sysctl_vfs_cache_pressure);
 
 static __cacheline_aligned_in_smp DEFINE_SPINLOCK(dcache_lru_lock);
