b47883f16f856129ad4831ca34ccb4659bfe2912
DeltaResero <deltaresero@zoho.com>
2014-02-08 10:03:38 -0400

    Disable console_suspend_enabled

--- kernel/printk.c
+++ kernel/printk.c
@@ -930,7 +930,7 @@ int update_console_cmdline(char *name, int idx, char *name_new, int idx_new, cha
 	return -1;
 }
 
-int console_suspend_enabled = 1;
+int console_suspend_enabled = 0;
 EXPORT_SYMBOL(console_suspend_enabled);
 
 static int __init console_suspend_disable(char *str)
