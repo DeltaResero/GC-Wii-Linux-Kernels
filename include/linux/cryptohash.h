2db0ea394517748d70a8456f87465e1d89777911
DeltaResero <deltaresero@zoho.com>
2014-02-07 23:02:43 -0400

    Git implementation of SHA-1

--- include/linux/cryptohash.h
+++ include/linux/cryptohash.h
@@ -2,7 +2,7 @@
 #define __CRYPTOHASH_H
 
 #define SHA_DIGEST_WORDS 5
-#define SHA_WORKSPACE_WORDS 80
+#define SHA_WORKSPACE_WORDS 16
 
 void sha_init(__u32 *buf);
 void sha_transform(__u32 *digest, const char *data, __u32 *W);
