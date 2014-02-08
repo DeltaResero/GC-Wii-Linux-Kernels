09ccc94af804e4bb190886a7154011a9d7d5aec9
DeltaResero <deltaresero@zoho.com>
2014-02-07 21:48:34 -0400

    Hold mmap_sem during page walk

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -714,8 +714,6 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 	down_read(&current->mm->mmap_sem);
 	ret = get_user_pages(current, current->mm, uaddr, pagecount,
 			     1, 0, pages, NULL);
-	up_read(&current->mm->mmap_sem);
-
 	if (ret < 0)
 		goto out_free;
 
@@ -764,6 +762,7 @@ out_pages:
 		page_cache_release(page);
 	}
 out_free:
+	up_read(&current->mm->mmap_sem);
 	kfree(pages);
 out_mm:
 	mmput(mm);
