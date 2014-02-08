77cedb6719c89c4b402cb706ce96d42cc0ae466b
DeltaResero <deltaresero@zoho.com>
2014-02-08 10:12:21 -0400

    Move REPEAT_BYTE definition

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -38,6 +38,8 @@ extern const char linux_proc_banner[];
 
 #define STACK_MAGIC	0xdeadbeef
 
+#define REPEAT_BYTE(x)	((~0ul / 0xff) * (x))
+
 #define ALIGN(x,a)		__ALIGN_MASK(x,(typeof(x))(a)-1)
 #define __ALIGN_MASK(x,mask)	(((x)+(mask))&~(mask))
 #define PTR_ALIGN(p, a)		((typeof(p))ALIGN((unsigned long)(p), (a)))
