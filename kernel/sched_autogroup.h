1cdb09f309ac43efd7add50ad97c445791e85f46
DeltaResero <deltaresero@zoho.com>
2014-02-08 18:18:08 -0400

    Fix missing line in autogroup header

--- kernel/sched_autogroup.h
+++ kernel/sched_autogroup.h
@@ -5,6 +5,8 @@ static void __sched_move_task(struct task_struct *tsk, struct rq *rq);
 static inline struct task_group *
 autogroup_task_group(struct task_struct *p, struct task_group *tg);
 
+static inline void autogroup_free(struct task_group *tg) { }
+
 #else /* !CONFIG_SCHED_AUTOGROUP */
 
 static inline void autogroup_init(struct task_struct *init_task) {  }
