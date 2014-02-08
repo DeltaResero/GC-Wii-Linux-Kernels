1a411b18bd4b9982644ad089d639126e6248d39b
DeltaResero <deltaresero@zoho.com>
2014-02-08 18:09:21 -0400

    Various tweaks

--- mm/filemap.c
+++ mm/filemap.c
@@ -1023,8 +1023,8 @@ static void do_generic_file_read(struct file *filp, loff_t *ppos,
 
 	for (;;) {
 		struct page *page;
-		pgoff_t end_index;
-		loff_t isize;
+		pgoff_t end_index = 0;
+		loff_t isize = 0;
 		unsigned long nr, ret;
 
 		cond_resched();
