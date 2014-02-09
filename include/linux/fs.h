4fbb6dc6a637c0a390946cacd6f0746336245c92
DeltaResero <deltaresero@zoho.com>
2014-02-08 21:12:29 -0400

    FS make size of chrdevs configurable

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2069,7 +2069,7 @@ extern void bd_release_from_disk(struct block_device *, struct gendisk *);
 #endif
 
 /* fs/char_dev.c */
-#define CHRDEV_MAJOR_HASH_SIZE	255
+#define CHRDEV_MAJOR_HASH_SIZE	CONFIG_CHRDEV_MAJOR_HASH_SIZE
 extern int alloc_chrdev_region(dev_t *, unsigned, unsigned, const char *);
 extern int register_chrdev_region(dev_t, unsigned, const char *);
 extern int __register_chrdev(unsigned int major, unsigned int baseminor,
