2df1b48a8f77f9a81c40544dc1db64da1311885e
DeltaResero <deltaresero@zoho.com>
2014-02-08 21:11:36 -0400

    Make COMMAND_LINE_SIZE configurable

--- arch/mips/include/asm/setup.h
+++ arch/mips/include/asm/setup.h
@@ -1,7 +1,7 @@
 #ifndef _MIPS_SETUP_H
 #define _MIPS_SETUP_H
 
-#define COMMAND_LINE_SIZE	4096
+#define COMMAND_LINE_SIZE	CONFIG_COMMAND_LINE_SIZE
 
 #ifdef  __KERNEL__
 extern void setup_early_printk(void);
