e263b6771016f0f26ddf16bfb1ad9f541c66fc08
DeltaResero <deltaresero@zoho.com>
2014-02-08 20:18:15 -0400

    fs simple fsync race fix

--- fs/libfs.c
+++ fs/libfs.c
@@ -826,11 +826,7 @@ int simple_fsync(struct file *file, struct dentry *dentry, int datasync)
 	int ret;
 
 	ret = sync_mapping_buffers(inode->i_mapping);
-	if (!(inode->i_state & I_DIRTY))
-		return ret;
-	if (datasync && !(inode->i_state & I_DIRTY_DATASYNC))
-		return ret;
-
+	
 	err = sync_inode(inode, &wbc);
 	if (ret == 0)
 		ret = err;
