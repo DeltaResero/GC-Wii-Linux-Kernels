06fb3fe954969e5db0e81e685abcb938a10d5327
DeltaResero <deltaresero@zoho.com>
2014-02-18 10:30:01 -0400

    Update build-gc-wii-kernel.sh

--- build-gc-wii-kernel.sh
+++ build-gc-wii-kernel.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-
 #
 # Linux Shell Script For Compiling GC/Wii Linux Kernels
 # Written by DeltaResero <deltaresero@zoho.com>
 #
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# on the rights to use, copy, modify, merge, publish, distribute, sub
+# license, and/or sell copies of the Software, and to permit persons to whom
+# the Software is furnished to do so, subject to the following conditions:
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 #
 
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without limitation
-#  on the rights to use, copy, modify, merge, publish, distribute, sub
-#  license, and/or sell copies of the Software, and to permit persons to whom
-#  the Software is furnished to do so, subject to the following conditions:
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
-#  ADAM JACKSON BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-#  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-#  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#
-#
 
 # Set defaults and checks if arch is supported by this script 
 clear
@@ -48,6 +45,8 @@ echo "- Processor Information -"
 echo "CPU Architecture: "${MACHINE_TYPE}
 numProcessors=$(grep -c ^processor /proc/cpuinfo)
 echo "Number of processors:" ${numProcessors}
+#------------------------------------------------------------------------------
+
 
 while : # Gets the user to select a console target (GameCube or Wii)
 do
@@ -78,6 +77,7 @@ do
        ;;
   esac
 done
+#------------------------------------------------------------------------------
 
 
 if [[ ${buildTarget} != 'Unknown' ]]; then
@@ -131,6 +131,36 @@ else
   printf "\n\nSelecting config: .config\n"
   useConfig='.config'
 fi
+#------------------------------------------------------------------------------
+
+
+# Sets firmware folder path
+FMW_DIRECTORY='lib/firmware'
+
+# Removes the firmware directory if it already exist
+if [[ -d "$FMW_DIRECTORY" ]]; then
+  echo "Removing the old contents from firmware folder..."
+  rm -R $FMW_DIRECTORY
+fi
+
+#Creates the firmware directory
+echo "Creating firmware folder / adding contents into:" $FMW_DIRECTORY
+mkdir $FMW_DIRECTORY
+
+
+# Sets the headers folder path
+HDR_DIRECTORY='usr/lib'
+
+# Removes the headers directory if it already exist
+if [[ -d "$HDR_DIRECTORY" ]]; then
+  echo "Removing the old contents from headers folder..."
+  rm -R $HDR_DIRECTORY
+fi
+
+#Creates the headers directory
+echo "Creating headers folder / adding contents into:" $HDR_DIRECTORY
+mkdir $HDR_DIRECTORY
+#------------------------------------------------------------------------------
 
 
 if [[ ${MACHINE_TYPE} == 'ppc' ]]; then
@@ -140,7 +170,14 @@ if [[ ${MACHINE_TYPE} == 'ppc' ]]; then
   fi
   make menuconfig
   make clean
-  make -j${numProcessors}
+  if [[ -f 'arch/powerpc/boot/ramdisk.image.gz' ]]; then
+    printf "Ramdisk found, building zImage.initrd...\n"
+    make firmware_install headers_install zImage.initrd -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY 
+  else
+    printf "No ramdisk found, building zImage...\n"
+    make firmware_install headers_install zImage -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY 
+  fi
+  printf "\nNote: If this is the target machine, it should be possible to install everything with 'sudo make install'."
 
 elif [[ ${MACHINE_TYPE} == 'x86_32' || ${MACHINE_TYPE} == 'x86_64' ]]; then
   while : # Gets the user to select a cross compiler to use
@@ -164,8 +201,13 @@ elif [[ ${MACHINE_TYPE} == 'x86_32' || ${MACHINE_TYPE} == 'x86_64' ]]; then
 
           make menuconfig ARCH=powerpc CROSS_COMPILE=H-i686-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
           make clean ARCH=powerpc CROSS_COMPILE=H-i686-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
-          make -j${numProcessors} ARCH=powerpc CROSS_COMPILE=H-i686-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
-
+          if [[ -f 'arch/powerpc/boot/ramdisk.image.gz' ]]; then
+            printf "Ramdisk found, building zImage.initrd...\n"
+            make firmware_install headers_install zImage.initrd -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc CROSS_COMPILE=H-i686-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+          else
+            printf "No ramdisk found, building zImage...\n"
+            make firmware_install headers_install zImage -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc CROSS_COMPILE=H-i686-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+          fi
         elif [[ ${MACHINE_TYPE} == 'x86_64' ]]; then
           export LD_LIBRARY_PATH=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/lib
 
@@ -175,7 +217,13 @@ elif [[ ${MACHINE_TYPE} == 'x86_32' || ${MACHINE_TYPE} == 'x86_64' ]]; then
 
           make menuconfig ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
           make clean ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
-          make -j${numProcessors} ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+          if [[ -f 'arch/powerpc/boot/ramdisk.image.gz' ]]; then
+            printf "Ramdisk found, building zImage.initrd...\n"
+            make firmware_install headers_install zImage.initrd -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+          else
+            printf "No ramdisk found, building zImage...\n"
+            make firmware_install headers_install zImage -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc CROSS_COMPILE=H-x86_64-pc-linux-gnu/cross-powerpc-linux-uclibc/usr/bin/powerpc-linux-
+          fi
         fi         
         break;;
 
@@ -186,7 +234,13 @@ elif [[ ${MACHINE_TYPE} == 'x86_32' || ${MACHINE_TYPE} == 'x86_64' ]]; then
 
         make menuconfig ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
         make clean ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
-        make -j${numProcessors} ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        if [[ -f 'arch/powerpc/boot/ramdisk.image.gz' ]]; then
+          printf "Ramdisk found, building zImage.initrd...\n"
+          make firmware_install headers_install zImage.initrd -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        else
+          printf "No ramdisk found, building zImage...\n"
+          make firmware_install headers_install zImage -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        fi
         break;;
 
       3) printf "\n\nQuitting script...\n"
@@ -217,7 +271,13 @@ else # !ppc && !x86_32 && !x86_64
 
         make menuconfig ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
         make clean ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
-        make -j${numProcessors} ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        if [[ -f 'arch/powerpc/boot/ramdisk.image.gz' ]]; then
+          printf "Ramdisk found, building zImage.initrd...\n"
+          make firmware_install headers_install zImage.initrd -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        else
+          printf "No ramdisk found, building zImage...\n"
+          make firmware_install headers_install zImage -j${numProcessors} INSTALL_FW_PATH=$FMW_DIRECTORY INSTALL_HDR_PATH=$HDR_DIRECTORY  ARCH=powerpc GCC_HOST=powerpc-linux-gnu- CROSS_COMPILE=powerpc-linux-gnu- CC="ccache powerpc-linux-gnu-gcc"
+        fi
         break;;
 
       2) printf "\n\nQuitting script...\n"
@@ -231,6 +291,7 @@ else # !ppc && !x86_32 && !x86_64
     esac
   done    
 fi
+#------------------------------------------------------------------------------
 
 
 # Placeholder for the path to the zImage/zImage.intrid binary
@@ -250,34 +311,18 @@ else # No zImage/zImage.intrid (was likely an unsuccessful build)
   exit 1
 fi
 
-# Sets modules folder path
-MOD_DIRECTORY='modules'
-
-# Removes modules directory if it already exist
-if [[ -d "$MOD_DIRECTORY" ]]; then
-  echo "Removing existing modules folder in current directory..."
-  rm -R $MOD_DIRECTORY
-fi
-
-#Creates the modules directory
-echo "Creating modules folder:" $MOD_DIRECTORY
-mkdir $MOD_DIRECTORY
-
-# Checks for modules and places them in a the modules folder
-find ! -path $MOD_DIRECTORY -name '*.ko' -exec cp -av {} $MOD_DIRECTORY \;
-
 while :
 do
   printf "\n\nReduce kernel size by removing extra debug data (stripping)?\n"
   printf "1) Yes, use SuperStrip (https://github.com/BR903/ELFkickers.git)\n"
-  printf "2) No, keep the zImage as is\n"
+  printf "2) No, keep the zImage / zImage.initrd as is...\n"
   echo -n "Response: "
   read opt5
   case $opt5 in
     1) printf "\n\nSuperStrip selected...\n"
       printf "Checking for an existing installation\n"
       if [[ ! -e 'type sstrip' ]]; then # Use local copy of sstrip
-        printf "SuperStrip found, stripping the debug info out of the zImage.\n"
+        printf "SuperStrip found, stripping the debug info out of the zImage / zImage.initrd.\n"
         sstrip -z $zImageFile
       else
         printf "SuperStrip not found, stripping the debug info out of the zImage will\n"
@@ -287,7 +332,7 @@ do
       fi
       break;;
 
-    2) printf "\n\nNot stripping zImage...\n"
+    2) printf "\n\nNot stripping zImage / zImage.initrd...\n"
       break;;
 
     *) printf "\n\n$opt is an invalid option.\n"
@@ -296,11 +341,46 @@ do
       read enterKey
       ;;
   esac
-done  
+done
+#------------------------------------------------------------------------------
 
-printf "\nDone! (Check to see if there were any errors above)\n"
+
+# Sets modules folder path
+MOD_DIRECTORY='lib/modules'
+
+# Removes modules directory if it already exist
+if [[ -d "$MOD_DIRECTORY" ]]; then
+  echo "Removing the old contents from modules folder..."
+  rm -R $MOD_DIRECTORY
+fi
+
+#Creates the modules directory
+echo "Creating modules folder / adding contents into:" $MOD_DIRECTORY
+mkdir $MOD_DIRECTORY
+
+# Checks for modules and places them in a the modules folder
+find ! -path $MOD_DIRECTORY -name '*.ko' -exec cp -av {} $MOD_DIRECTORY \;
+#------------------------------------------------------------------------------
+
+
+# Script finish message
+printf "\nDone! (Check to see if there were any errors above)\n\n"
 printf "The kernel (zImage) can be found in: 'arch/powerpc/boot'\n"
+printf "Although the inclusion of headers, modules, and firmware are mostly non essential for basic use,\n"
+printf "it's recommended to include these.  At minimal, any built modules and the firmware should be\n"
+printf "included in their folders on the target device in the /lib/ folder (/lib/modules and /lib/firmware).\n\n"  
+echo "Kernel headers should be located in the folder:" $HDR_DIRECTORY
+printf "\nUsually kernel headers can be installed manually or by using a package manager instead...\n"
+echo "Firmware (if any) should be located in the folder:" $FMW_DIRECTORY
+echo "The b43 (WLAN) Wii firmware must be built / retreived seperate, see the following site for"
+printf " more info: http://www.gc-linux.org/wiki/WL:Wifi_Configuration\n"
+printf "\nFirmware should be placed into the '/lib/firmware' folder of the target system.\n"
 echo "Modules (if any) should be located in the folder:" $MOD_DIRECTORY
+printf "\nModules should be placed into the '/lib/modules/KERNEL_VERSION_NUMBER' folder of\n"
+printf "the target system where KERNEL_VERSION_NUMBER is the numerical version of the kernel.\n"
+printf "\nWARNING: If using a ramdisk based kernel, remember to add the new modules / firmware \n"
+printf "into their appropriate folders in the ramdisk image and rebuild the kernel (with the\n"
+printf "same configuration) with the new ramdisk image.\n" 
 exit 0
 #
 # More info on the gcLinux cross compile tool can be found at the following website:
